28fb0379b0b6810cc5997a625edc6d7019e19f5f
Rafael <rafinha_fern@hotmail.com>
2023-03-19 23:04:12 -0300

    feat: add new avatar react component and stories docs.

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,18 @@
+import { AvatarContainer, AvatarFallback, AvatarImage } from "./styles.js";
+import { User } from 'phosphor-react'
+import { ComponentProps } from "@stitches/react";
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage>{
+}
+
+
+export function Avatar(props:AvatarProps) {
+   return (
+    <AvatarContainer>
+        <AvatarImage {...props}/>
+        <AvatarFallback delayMs={600}>
+            <User/>
+        </AvatarFallback>
+    </AvatarContainer>
+    )
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,4 @@
 export * from './components/Box.js'
 export * from './components/Text.js'
 export * from './components/Heading.js'
+export * from './components/Avatar'
