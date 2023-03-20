4ec36df5c439666270271fba474877d6b34aab56
Rafael <rafinha_fern@hotmail.com>
2023-03-20 01:49:19 -0300

    feat: add checkbox component and stories.

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from "@stitches/react";
+import { Check } from "phosphor-react";
+import { CheckboxContainer, CheckboxIndicator } from "./styles.js";
+
+
+export function Checkbox(props:CheckboxProps) {
+    return(
+        <CheckboxContainer {...props}>
+            <CheckboxIndicator asChild={true}>
+                <Check weight="bold"/>
+            </CheckboxIndicator>
+        </CheckboxContainer>
+    )
+}
+
+export interface CheckboxProps extends ComponentProps<typeof CheckboxContainer> {}
