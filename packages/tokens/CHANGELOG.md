b2a59ee0037bd71e4cc2c9f1b20373811b34e267
Rafael <rafinha_fern@hotmail.com>
2023-03-21 01:28:36 -0300

    chore: Add new test color on tokens package.

---
+++ packages/tokens/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @raferdev_study/tokens
+
+## 1.1.0
+
+### Minor Changes
+
+- Add new color : test
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -15,4 +15,5 @@ export const colors = {
     ignite500: '#00875F',
     ignite700: '#015F43',
     ignite900: '#00291D',
+    test:'#fff'
   }
